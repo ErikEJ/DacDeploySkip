877fe5105543ce00a1789f58e9f2a5497ed0247a
Erik Ejlskov Jensen <eej@delegate.dk>
2025-10-17 16:33:32 +0200

    fixes #6
    Add XmlRewriter, tests, and pipeline integration
    
    Updated the solution to include a new `Test.csproj` project for unit testing. Introduced the `XmlRewriter` class to handle metadata rewriting in `model.xml`. Refactored `DacpacChecksumService` to use `XmlRewriter` for checksum calculations.
    
    Enhanced the `readme.md` with pipeline integration examples, demonstrating conditional deployment logic.
    
    Bumped the package version to `1.0.7`

---
+++ Test/RewriteTest.cs
@@ -0,0 +1,41 @@
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Test
+{
+    public class RewriteTest
+    {
+        [Fact]
+        public async Task TestRewrite()
+        {
+            // Arrange
+            var rewriter = new DacDeploySkip.XmlRewriter();
+
+            // Act
+            await rewriter.RewriteXmlMetadataAsync("model.xml");
+
+            // Assert
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load("model.xml");
+
+            var nodes = xmlDoc.GetElementsByTagName("Metadata");
+
+            foreach (XmlNode node in nodes)
+            {
+                if (node.Attributes == null)
+                {
+                    continue;
+                }
+
+                if (node.Attributes != null 
+                    && node.Attributes.Count == 2
+                    && node.Attributes[0].Name == "Name"
+                    && node.Attributes[0].Value == "FileName")
+                {
+                    var originalValue = node.Attributes[1].Value;
+                    Assert.True(!originalValue.Contains(Path.DirectorySeparatorChar));
+                }
+            }
+        }
+    }
+}
---
+++ src/DacDeploySkip/XmlRewriter.cs
@@ -0,0 +1,54 @@
+﻿using System.Xml;
+
+namespace DacDeploySkip
+{
+    /// <summary>
+    /// This class is for internal use only.
+    /// </summary>
+    public class XmlRewriter
+    {
+        public async Task RewriteXmlMetadataAsync(string modelFile)
+        {
+            const string fileKey = "FileName";
+            const string symbolsKey = "AssemblySymbolsName";
+            var contents = await File.ReadAllTextAsync(modelFile);
+
+            if (!contents.Contains($"<Metadata Name=\"{fileKey}\" ", StringComparison.Ordinal)
+                && !contents.Contains($"<Metadata Name=\"{symbolsKey}\" ", StringComparison.Ordinal))
+            {
+                return;
+            }
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(contents);
+
+            var nodes = xmlDoc.GetElementsByTagName("Metadata");
+
+            foreach (XmlNode node in nodes)
+            {
+                if (node.Attributes == null)
+                {
+                    continue;
+                }
+
+                ReplaceValue(fileKey, node);
+
+                ReplaceValue(symbolsKey, node);
+            }
+
+            xmlDoc.Save(modelFile);
+        }
+
+        private static void ReplaceValue(string key, XmlNode metaData)
+        {
+             if (metaData.Attributes != null 
+                && metaData.Attributes.Count == 2
+                && metaData.Attributes[0].Name == "Name"
+                && metaData.Attributes[0].Value == key)
+            {
+                var originalValue = metaData.Attributes[1].Value;
+                metaData.Attributes[1].Value = Path.GetFileName(originalValue);
+            }
+        }
+    }
+}
